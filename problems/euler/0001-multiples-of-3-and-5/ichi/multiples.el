3acf4923626f184e09c8153f3431880bd8f06a42
Craig Ferry <craig@puritycontrol.net>
2016-02-14 09:40:51 +0000

    Convert mulitples_to into an interactive function.

--- problems/euler/0001-multiples-of-3-and-5/ichi/multiples.el
+++ problems/euler/0001-multiples-of-3-and-5/ichi/multiples.el
@@ -3,10 +3,15 @@
 (require 'cl-lib)
 
 (defun multiples_to (limit)
-  (apply '+ (cl-remove-if-not
-             (lambda (x) (or (zerop (% x 3))
-                             (zerop (% x 5))))
-             (number-sequence 1 (1- limit)))))
+  (interactive "n What is the limit you want to sum multiples to?: ")
+  (let ((answer (apply '+ (cl-remove-if-not
+                           (lambda (x) (or (zerop (% x 3))
+                                           (zerop (% x 5))))
+                           (number-sequence 1 (1- limit))))))
+    (message "The sum of multiples of 3 and 5 up to %d is %d"
+             limit
+             answer)
+    answer))
 
 
 (provide 'multiples)
